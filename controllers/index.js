df0eef25bf7b0f4e5caa3c570554a1ba43f1b975
BigNATE38 <natevandy38@gmail.com>
2023-04-30 16:44:44 -0700

    pseudo code for routes and javascript in the public folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
+// const apiRoutes = require("./api");
 const homeRoutes = require('./homeRoutes.js');
+// const dashboardRoutes = require("./dashboardRoutes")
 
 router.use('/', homeRoutes);
+// router.use("/dashboard", dashboardRoutes);
+// router.usd("/api", apiRoutes);
 
 module.exports = router;
